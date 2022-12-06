e47eda36547fe64c087c56b5c0d36d64d7ba9a4e
Pauló Lajos <paulolajos@mail.com>
2022-12-06 18:09:19 +0100

    chapter 3...

--- chpt_03a.kts
+++ chpt_03a.kts
@@ -10,5 +10,32 @@ val totalCost: Double = hourlyRate * hoursWorked
 
 val characterA: Char = 'a'
 
-val stringDog: String = "Dog"
+//val stringDog: String = "Dog"
+val stringDog = "Dog" 
+// Inferred to be of type String
+
+var message = "Hello" + " my name is " 
+val name = "Dick" 
+message += name 
+// "Hello my name is Dick"
+val exclamationMark: Char = '!' 
+message += exclamationMark 
+// "Hello my name is Dick!"
+
+message = "Hello my name is $name!" // "Hello my name is Dick!"
+
+val oneThird = 1.0 / 3.0 
+val oneThirdLongString = "One third is $oneThird as a decimal."
+val oneThirdLongString2 = "One third is ${1.0 / 3.0} as a decimal."
+
+val bigString = """ 
+|You can have a string 
+|that contains multiple 
+|lines 
+|by 
+|doing this. 
+""".trimMargin() 
+println(bigString)
+
+//edited with spacevim
 
---
+++ chpt_03b.kts
@@ -0,0 +1,4 @@
+val coordinates:Pair<Int, Int> =Pair(2, 3)
+println(coordinates)
+
+val coordinatesInferred = Pair(3, 4)
